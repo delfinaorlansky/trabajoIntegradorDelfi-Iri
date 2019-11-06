32b0bd1f4d4af05cfd77714a49df331d9b6d464d
Delfina Orlansky <dorlansky@udesa.edu.ar>
2019-11-06 12:08:44 -0300

    .

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.addEventListener('load',function(){
+  //
+var api_key = "60be6317b0012697cb25d6ce427de920"
+fetch("https://api.themoviedb.org/3/tv/top_rated?api_key="+api_key+"&language=en-US&page=1")
+.then(function(response) {
+ return response.json()
+})
+.then(function(information) {
+  var url = "https://image.tmdb.org/t/p/original"
+  var titulo = ""
+  var poster = ""
+  var serie = ""
+  var ul = document.querySelector('ul#mayorPuntaje')
+  for (var i = 0; i < information.results.length; i++) {
+    titulo = information.results[i].name
+    console.log(information.results[i].poster_path);
+    poster = url + information.results[i].poster_path
+
+    serie = '<li>'
+    serie +=   '<img src="'+poster+'" alt="">'
+    serie +=   '<div class="uk-position-center uk-panel"><h2 class="">'+titulo+'</h2></div>'
+    serie += '</li>'
+
+    console.log(serie);
+    ul.innerHTML += serie
+  }
+})
+.catch(function(error) {
+ console.log("Error: " + error);
+})
