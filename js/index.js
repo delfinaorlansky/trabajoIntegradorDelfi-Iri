f72acce9460cde1ca124850c56642b311ed40085
Delfina Orlansky <dorlansky@udesa.edu.ar>
2019-11-08 14:37:05 -0300

    agregue carrusel de series mas populares

--- js/index.js
+++ js/index.js
@@ -5,31 +5,27 @@ window.addEventListener('load',function(){
   var urlMasPopulares = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key + "&language=en-US&page=1"
   var urlMayorPuntaje = "https://api.themoviedb.org/3/tv/top_rated?api_key="+api_key+"&language=en-US&page=1"
   var urAlAire = "https://api.themoviedb.org/3/tv/airing_today?api_key="+api_key+"&language=en-US&page=1"
+
   fetch(urlMayorPuntaje)
     .then(function(response) {
       return response.json() // el servidor me responde con un string en formato JSON, gracias a eso lo transformo en un objeto literal.
     })
     .then(function(datos) {
-      console.log(datos);
       var url = "https://image.tmdb.org/t/p/original/"
-      var titulo = ""
-      var poster = ""
-      var serie = ""
-      var contenedorSeriesMayorPuntaje = document.querySelector('#mayorPuntaje')
+      var contenedorSeriesMayorPuntaje = document.querySelector('#mayorPuntaje ul')
+
+      var contenidoSlider = '';
 
       for (var i = 0; i < 10; i++) {
-        titulo = datos.results[i].name
-        console.log(datos.results[i].poster_path);
         poster = url + datos.results[i].poster_path
-
-        serie = '<article class="article-series">'
-        serie +=   '<img src="'+poster+'" width="100px">'
-        //serie +=   '<h2 class="titulo-series">'+titulo+'</h2>' //
-        serie += '</article>'
-
-        console.log(serie);
-        contenedorSeriesMayorPuntaje.innerHTML += serie
+        contenidoSlider += `
+          <li>
+              <img src="${poster}" alt="">
+          </li>
+        `;
       }
+
+      contenedorSeriesMayorPuntaje.innerHTML = contenidoSlider
     })
     .catch(function(errores) {
         console.log("Error: " + errores);
