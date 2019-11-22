612d50c727c7102924c39cf8e3ebacfc301d5464
Delfina Orlansky <dorlansky@udesa.edu.ar>
2019-11-21 22:03:44 -0300

    segui con recomendaciones

--- js/detalle.js
+++ js/detalle.js
@@ -39,11 +39,49 @@ window.addEventListener('load',function(){
       listaResultados.innerHTML += contenidoFinal;
 })
 })
+// RECOMENDACIONES
+var queryStringObj = new URLSearchParams(window.location.search);
+var urlFija = "https://image.tmdb.org/t/p/original/";
+function createItemHtml(clase, titulo, imagen, idSerie) {
 
-      document.querySelector ("#recomendaciones").addEventListener("click", function () {
-      fetch("https://api.themoviedb.org/3/movie/" + idSerie + "/recommendations?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
-      .then(function(response) {
-        return response.json();
-      })
+  serie = '<li class="'+ clase + '">'
+  serie +=     '<a href="detalles.html?idSerie='+idSerie+'&nombreSerie='+titulo+'" >'
+  serie +=      '<h2>'+ titulo +'</h2>'
+  serie +=      '<img src="'+ imagen +'" alt="">'
+  serie +=    '</a>'
+  serie +=  '</li>';
 
-      })
+  return serie;
+}
+
+var idSerie = queryStringObj.get('idDeSerie');
+var urlRecomendaciones = "https://api.themoviedb.org/3/tv/"+idSerie+"/recommendations?api_key=81abb78b34be12fc4620b0a001276f5a&language=en-US&page=1"
+   fetch(urlRecomendaciones)
+     .then(function(response) {
+       return response.json();
+     })
+     .then(function(resultados) {
+       console.log(resultados);
+       var recoContainer = document.querySelector(".reco");
+         for (var i = 0; i < resultados.results.length; i++) {
+           titulo = resultados.results[i].name
+           img_src = urlFija + resultados.results[i].poster_path
+
+           var serie = createItemHtml("reco-items", titulo, img_src, resultados.results[i].id) ;
+           recoContainer.innerHTML += serie
+         }
+
+       })
+
+var sectionReco = document.querySelector(".sectionReco")
+var recoBoton = document.getElementById("recomendacionesBoton")
+ recoBoton.onclick = function onclick(){
+ sectionReco.style.display = "block";
+ console.log(sectionReco.style.display);
+ console.log(recoBoton);
+
+}
+
+    //  document.querySelector ("#recomendaciones").addEventListener("click", function () {
+    // fetch("https://api.themoviedb.org/3/movie/" + idSerie + "/recommendations?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
+    // })
